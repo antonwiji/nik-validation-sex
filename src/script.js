39a3bd0801c660dbbc398dc23a199eab43b644b4
Anton <wijayaanton50@gmail.com>
2024-03-03 19:29:48 +0700

    first commit

---
+++ src/script.js
@@ -0,0 +1,27 @@
+const getSex = (nik) => {
+    return nik.substring(6, 8)
+  }
+  
+const validationSex = (nik) => {
+    
+    const getSexTemp = getSex(nik)
+    if(getSexTemp >= 41) {
+
+      return 'Perempuan'
+    }
+
+    return 'Laki - Laki '
+  }
+
+const onClickButton = () => {
+    const valueNik = document.getElementById('valueNik').value
+
+    const resultSex = validationSex(valueNik)
+
+    if(valueNik.length >= 16 ){
+        document.getElementById('alertResult').innerHTML = `<div class="alert alert-success" role="alert">
+        Nik Dengan ${valueNik} Jenis Kelaminnya adalah <b>${resultSex}</b> 
+        </div>`
+    }
+
+}
